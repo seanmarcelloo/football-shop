83a8104ddbd584e8b3f989cc5f2ec59b83dc64a4
seanmarcelloo <seanmarcello836@gmail.com>
2025-09-09 19:25:25 +0700

    Menambahkan url pada proyek

--- main/models.py
+++ main/models.py
@@ -1,32 +1,22 @@
-import uuid
 from django.db import models
 
-class News(models.Model):
+class Product(models.Model):
     CATEGORY_CHOICES = [
-        ('transfer', 'Transfer'),
-        ('update', 'Update'),
-        ('exclusive', 'Exclusive'),
-        ('match', 'Match'),
-        ('rumor', 'Rumor'),
-        ('analysis', 'Analysis'),
+        ('jersey', 'Jersey'),
+        ('shorts', 'Shorts'),
+        ('shoes', 'Football Shoes'),
+        ('ball', 'Football'),
+        ('equipment', 'Training Equipment'),
+        ('accessories', 'Accessories'),
+        ('merchandise', 'Merchandise'),
     ]
     
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
-    news_views = models.PositiveIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
     is_featured = models.BooleanField(default=False)
     
     def __str__(self):
-        return self.title
-    
-    @property
-    def is_news_hot(self):
-        return self.news_views > 20
-        
-    def increment_views(self):
-        self.news_views += 1
-        self.save()
+        return self.name
