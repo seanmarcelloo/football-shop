acc5d8406fff37d88260399e362132856d919407
seanmarcelloo <seanmarcello836@gmail.com>
2025-10-07 15:32:41 +0700

    Mengubah id product dengan menggunakan uuid

--- main/models.py
+++ main/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -15,6 +16,7 @@ class Product(models.Model):
     ]
 
     # uuid = models.UUIDField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
